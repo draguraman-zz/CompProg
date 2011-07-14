81381facfc362b83f893a1a16367ace56a38127b
Lucas Tan <tbhtan3@gmail.com>
2011-07-14 11:14:16 -0400

    solved p822
    Change-Id: I271418e5f0a939be117153565c068f4239519c69

--- UVa/finals/2000/p822.c
+++ UVa/finals/2000/p822.c
@@ -1,5 +1,19 @@
+/*
+ * Algorithm:
+ * Priority Queues
+ * 
+ * - First we generate all requests and push them into a prio q.
+ * - Then pop them one by one and handle.
+ * - The tricky case is when two requests appear at the same time.
+ *   Have to use the conflict-resolving logic.
+ *
+ * Gotchas:
+ * - Language not very clear in the description (actually including mine :)
+ * - Time between requests includes servicing time.
+ *
+ * */
 
-
+#include<vector>
 #include<map>
 #include<queue>
 #include <stdio.h>
@@ -15,15 +29,13 @@ input:
 0
 */
 
-
-std::map<int,int> timeneed;
-
 typedef struct
 {
     int time; // time at which this req appears , minutes since start
     int id;
 } req_t;
 
+// job time ordering logic
 class reqcomp
 {
     public: bool operator()(const req_t &a,const req_t&b)
@@ -32,65 +44,109 @@ class reqcomp
     }
 };
 
-std::priority_queue<req_t, std::vector<req_t>, reqcomp> q;
-
 typedef struct
 {
     int id;
     int last_job; // time of last job scheduled time
     int free; // time at which person is free
-    int topics[999999]; // temrinated by -1
-    
+    std::map<int,int> prio; // topic id to prio
 } person_t;
+
+typedef struct
+{
+    int ridx;
+    int pid;
+}job_t;
+
 
 person_t persons[10];
 int numpersons;
 
-/*
-int reqcomp(const void * a, const void * b)
-{
-    return ((req_t*)a)->time - ((req_t*)b)->time;
-}*/
+typedef std::priority_queue<req_t, std::vector<req_t>, reqcomp> qt;
+std::priority_queue<req_t, std::vector<req_t>, reqcomp> *q;
+std::map<int,int> timeneed; // id to time needed
+std::vector<req_t> list;
+std::vector<bool> done;
 
-int select (int rid)
+// job scheduling and conflict -resolving  logic
+class jobcomp
 {
-    int id = reqs[rid].id;
-    int time = reqs[rid].time;
-    int i,j;
-    int idx = -1;
-    int prio = 0x7fffffff;
-    for(i=0;i < numpersons;i++){
-        if (persons[i].free <= time)
-        {
-            // if per is free, grab his prio forthis topic
-            for (j = 0;-1!= persons[i].topics[j];j++){
-                if (persons[i].topics[j] == id)
-                {
-                    if (j < prio){ idx = i; prio = j; }
-                    else if (j == prio )
-                    {
-                        if (persons[i].last_job < persons[idx].last_job)
-                        {
-                            idx=i;
-                        }
-                        else if(persons[i].last_job==persons[idx].last_job)
-                        {
-                                   
-                            if (i < idx){ idx=i;  }
-                        }
-                    }break;
-                }
+    public: bool operator()(const job_t &a,const job_t&b)
+    const {
+         person_t &pa = persons[a.pid];
+         person_t &pb = persons[b.pid];
+        int k= pa.prio[list[a.ridx].id] - pb.prio[list[b.ridx].id];
+        if (k != 0) return k > 0;
+        k = pa.last_job - pb.last_job;
+        if (k!=0) return k>0;
+        k = pa.id - pb.id;
+        if (k!=0) return k>0;
+        return true;
+    }
+};
+
+
+void handle(int time){
+    
+    std::priority_queue<job_t,std::vector<job_t>, jobcomp> jobq;
+    int i;
+    // loop thru each person and each job and push to jobq
+    for (i=0;i<numpersons;i++){
+        if (persons[i].free > time) continue;
+        for (int j = 0; j < list.size();j++){
+            const req_t & r = list[j];
+            if (persons[i].prio.find(r.id) != persons[i].prio.end())
+            {
+                job_t jo;
+                jo.ridx = j;
+                jo.pid = i;
+
+                jobq.push(jo);
             }
         }
     }
 
-    return idx;
-}
+    int numdone=0;
+    while (jobq.size())
+    {
+        job_t j = jobq.top(); jobq.pop();
+        const req_t & r = list[j.ridx];
 
-int handle(int rid){
-    int i,j;
-    int id = reqs[rid].id;
-    int time = reqs[rid].time;
+        // skip if job already done
+        if (done[j.ridx]) continue;
+        person_t & p = persons[j.pid];
+
+        // skip if person already has job
+        if (p.free > time) continue;
+        
+        p.last_job = time;
+        p.free = time + timeneed[r.id];
+        done[j.ridx] = true;
+        numdone++;
+    }
+
+    // any leftoverj  jobs?
+    if (numdone == list.size()) return;
+
+    // "reschedule" the jobs for another time
+    int minfree = 0x7fffffff;
+    for (i=0;i<numpersons;i++){
+        const person_t & p = persons[i];
+
+        // if a person is free at this time 
+        // that means he has no priority for any of these leftover
+        //  jobs
+        if (p.free < minfree && p.free > time) minfree = p.free;
+    }
+
+    // repush back to queue
+    for (i = 0 ; i < done.size();i++){
+        if (done[i]) continue;
+        req_t r;
+        r.id = list[i].id;
+        r.time = minfree;
+        q->push(r);
+    }
 
 }
 
@@ -105,53 +161,56 @@ int main()
     if (numtopics == 0) break;
 
     numpersons=0;
-    numreq=0;
-    total=0;
     int i,j,k;
     timeneed.clear();
-    q.clear();
+
+    delete q; // sigh... c++ prio q has no .clear() method...
+    q = new qt();
+
     for (i=0;i < numtopics; i++){
         int id, req, first, need, btw;
         scanf("%d%d%d%d%d", &id, &req, &first, &need, &btw);
         timeneed[id]=need;
+
+        // Generate all requests for this topic
         for (j = 0; j <req;j++){
-            reqs[numreq].id =id;
-            reqs[numreq].time = first;
-            first += need+btw;
-            numreq++;
+            req_t r;
+            r.id =id;
+            r.time = first;
+            first += btw;
+            q->push(r);
         }
     }
     scanf("%d",&numpersons);
     for (i=0;i < numpersons;i++){
         int id, num;
+        persons[i].prio.clear();
         scanf("%d%d",&id,&num);
         persons[i].id=id;
         persons[i].free = persons[i].last_job=0;
         for (j=0;j<num;j++){
-            scanf("%d",& persons[i].topics[j]);
+            int tid;
+            scanf("%d",& tid);
+            persons[i].prio.insert(std::make_pair(tid,j));
         }
-        persons[i].topics[j] = -1;
 
     }
 
-// todo: what if two  requests of diff type appear?
-
-    qsort( reqs, numreq, sizeof(req_t), reqcomp);
-
-    int delta = 0; // time to push back each req by
-    for (i=0;i<numreq;i++){
-        int pid = select(i);
-
-        int time = reqs[i].time+delta;
-        int id = reqs[i].id;
-        if (pid >=0){
-            persons[pid].last_job = time;
-            persons[pid].free = time + timeneed[id];
+    while(q->size()){
+        list.clear();
+        done.clear();
+        req_t r = q->top();
+        q->pop();
+        list.push_back(r);
+        done.push_back(false);
+        // Group all requests having same time together
+        while (q->size() && q->top().time==r.time){
+            list.push_back(q->top());
+            done.push_back(false);
+            q->pop();
         }
-        else{
-            // no one is free or no suitable per found
-            delta+=handle(i);
-        }
+
+        handle(list[0].time);
     }
 
     int max = 0;
