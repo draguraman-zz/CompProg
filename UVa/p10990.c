9d39d8aa0b71ef441624bb4d2f189743f97fdc61
Lucas Tan <tbhtan3@gmail.com>
2011-07-25 20:45:25 -0400

    solved p10990

--- UVa/p10990.c
+++ UVa/p10990.c
@@ -1,56 +1,22 @@
+/**
+Algorithm:
+Totient function + aggressive caching
+
+Used the formulae:
+- phi(p^a) = p^a-p^(a-1) where p prime
+- phi(n) = phi(p^i) * phi(q^j) ... where p, q .. are prime factors of n
+
+Gotchas:
+- phi(1)==1
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-int cache[2000001];
-int depthc[2000001];
+int cum[2000001];      // cumulative sums of depths
+int cache[2000001];    // phi values
+int depthc[2000001];   // depth cache
 char notprime[2000001];
-int primes[1500000];
-int nprimes;
-
-int gcd(int m, int n){
-
-    while (1){
-    if (n==0||m==0) return n+m;
-      int a = m;
-      m = n%m;
-      n = a;
-    }
-}
-
-int phi(int v){
-    if (cache[v]) return cache[v];
-    if (!notprime[v]) return v-1;
-
-    int i,j;
-
-    for(i=0;i<nprimes;i++){
-        if (0== v % primes[i]) break;
-    }
-
-    int d =gcd(primes[i], v/primes[i]);
-  int aa=  cache[v/primes[i]]= phi(v/primes[i]) ;
- int bb=   cache[primes[i]] = phi(primes[i]) ;
- int cc =  cache[d] = phi(d);
-
- return aa*bb*d/cc;
-
-/*
-    int ret= 1;
-    int v2 = v;
-    
-    for (i=0;i< nprimes;i++){
-        int p = primes[i];
-        if ( v2 < p) break;
-        if (v2 % p) continue;
-        int c = 0;
-        int add = 1;
-        while (v2 %p==0){add*= p;v2/=p;c++;}
-        ret *= add-add/p;
-    }
-    return cache[v]=ret;
-    */
-}
 
 void init()
 {
@@ -58,38 +24,48 @@ void init()
     int i,j;
     for (i=2; i < sizeof(notprime);i++){
         if (notprime[i]) continue;
-        primes[nprimes++]=i;
+        cache[i] = i-1;
+
+        // update each phi(j) while we update notprime
         for (j= i*2; j< sizeof(notprime); j+=i){
             notprime[j] = 1;
+            int o = j;
+            while (j % i ==0){j /= i;}
+             int add = o/j;
+             j=o;
+            if  (cache[j]==0)cache[j] = add-add/i;
+            else cache[j] *= add-add/i;
         }
     }
-}
-
-int depth(int n){
-    if (depthc[n])return depthc[n];
-    int p = phi(n);
-    int d = 1;
-    while (p > 1){
-        if (depthc[p])return d+ depthc[p];
-        p = phi(p); d++;
-    }
-    return depthc[n]=d;
 }
 
 int main()
 {
+    // The initialization is actually quite fast!
     init();
+    int k;
+    cum[0]=0;
+    cum[1]=1;
+    cum[2] = 2;
+    depthc[1] = depthc[2] = 1;
+    for (k = 3; k <= 2000000;k++){
+        int phi = cache[k];
+        int depth = 1+depthc[phi];
+        depthc[k] = depth;
+        cum[k] = cum[k-1]+ depth;
+    }
+
     int nu;scanf("%d", &nu);
     while (nu--){
         int a,b;
         scanf("%d%d",&a,&b);
-long long d=0;
-        int i,j;
+
+        // swap a & b if necessary
         if (a >b){ int t = a; a = b; b = t;}
-        for (i=a; i <= b;i++){
-          d += depth(i);
-        }
-        printf("%lld\n",d);
+
+        // constant time lookup!
+        int d = cum[b]-cum[a-1];
+        printf("%d\n",d);
     }
 }
 
