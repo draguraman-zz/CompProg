08410ae92204330eb4e7280cedea79215179f230
Lucas Tan <tbhtan3@gmail.com>
2012-04-01 03:36:35 -0400

    edited comments

--- codejam/2008-round2/rle.cpp
+++ codejam/2008-round2/rle.cpp
@@ -4,20 +4,25 @@
  */
 #include<iostream>
 #include<string.h>
-using namespace std;
 #include<cstdio>
+using namespace std;
+
 string str;
 int k;
 int w[17][17][17]; // w[t][i][j] == weight of edge i->j where t is last
 
 int dp[1<<16][17]; // dp[i][j] == length of path starting from j
-                   // visiting every node in i, and ending at 0
+                   // visiting every node in i, and ending at t
 
 char has[1<<16][17];
 
+// nodes : bitmask of nodes to visited, including x
+// x : current node
+// t : last node
 int find(int nodes, int x, int t){
     if (has[nodes][x])return dp[nodes][x];
     
+    // at last node?
     if (nodes == 1<<x){
         has[nodes][x]=1;
         return dp[nodes][x] = w[t][x][t];
@@ -68,7 +73,7 @@ int main(){
 
         int mm = 0x7fffffff;
         for (t = 0; t < k;t++){
-        memset(has,0,sizeof has);
+            memset(has,0,sizeof has);
             mm=min(mm, find((1<<k)-1, t, t));
         }
         printf("Case #%d: %d\n", casen++, mm+1);
