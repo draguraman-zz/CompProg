d8178fa4a5d828fa8c3cdf606757d5a91750116e
Lucas Tan <tbhtan3@gmail.com>
2014-08-23 15:42:39 +0800

    removed useless code

--- USACO/nuggets.cpp
+++ USACO/nuggets.cpp
@@ -30,12 +30,6 @@ using namespace std;
 char poss[256];
 int sizes[11], n;
 
-int gcd(int a, int b){
-    if (a==1 || b == 1)return 1;
-    if(a%b==0)return b;
-    return gcd(b, a%b);
-}
-
 int main(){
     ifstream in("nuggets.in");
     ofstream out("nuggets.out");
