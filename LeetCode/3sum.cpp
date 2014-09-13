b54c847093e9c71716ba8f09139db9f1fbb7c76f
Lucas Tan <tbhtan3@gmail.com>
2014-09-13 15:46:39 +0800

    improved 3sum

--- LeetCode/3sum.cpp
+++ LeetCode/3sum.cpp
@@ -1,53 +1,44 @@
+/**
+ * Algorithm is O(n^2) without using extra space except to store the solution.
+ */
 #include<iostream>
-#include<unordered_set>
-#include<unordered_map>
 #include<vector>
 #include<algorithm>
 using namespace std;
 
-typedef unordered_map<int,int> intmap;
-typedef pair<int,pair<int, int> > triplet;
-typedef unordered_set<triplet> tripletset;
-
-namespace std{
-    template<>
-    struct hash<triplet>{
-        inline size_t operator()(const triplet&p)const{
-            return p.first^p.second.first^p.second.second;
-        };
-    };
+struct comp{
+    inline bool operator()(const int&a, const int&b)const{
+        return a<b;
+    }
 };
 
 class Solution {
 public:
     vector<vector<int> > threeSum(vector<int> &num) {
-        intmap imap;
-        tripletset tset;
+        sort(num.begin(),num.end(),comp());
         vector<vector<int> > ans;
-        int i,j,k;
+        int i,j,prev1;
         const int n=num.size();
-        for(i=0;i<n;i++){
-            pair<intmap::iterator,bool> p=imap.emplace(make_pair(num[i],1));
-            if(!p.second)
-                ((p.first)->second)++;
-        }
-        vector<int> v;
-        for(i=0;i<n;i++){
+        for(i=0;i<n;prev1=num[i],i++){
             int a=num[i];
-            for(j=i+1;j<n;j++){
-                int b=num[j],c=-a-b;
-                intmap::iterator it=imap.find(c);
-                if(it==imap.end())continue;
-                int f=(a==c)+(b==c);
-                if(it->second<=f)continue;
-                v.clear();
-                v.push_back(a); v.push_back(b); v.push_back(c);
-                sort(v.begin(),v.end());
-                triplet t = make_pair(v[0],make_pair(v[1],v[2]));
-                pair<tripletset::iterator,bool> ret2=tset.insert(t);
-                if(ret2.second){
-                    ans.push_back(v);
+            // skip duplicates to avoid duplicate triplets.
+            if(i&&a==prev1)continue;
+            // below is essentially the O(n) method for 2sum,
+            // with duplicate detection.
+            int i1=i+1,i2=n-1,prev2,prev3=num[i2]+1;
+            while(i1<i2){
+                int b=num[i1],c=num[i2],sum=a+b+c;
+                if(sum>0)i2--;
+                else if(sum<0)i1++;
+                else {
+                    if (b!=prev2||c!=prev3){
+                        ans.emplace_back();
+                        ans.back()={a,b,c};
+                    }
+                    i1++;
                 }
+                prev2=b;
+                prev3=c;
             }
         }
         return ans;
@@ -56,7 +47,7 @@ public:
 
 int main(){
     Solution s;
-    vector<int> num={-1,0,1,2,-1,-4};
+    vector<int> num={-1,-1,-1,-2,2,0,0,0,1,2,-1,1,2,-1,-4};
     int i,j,k;
     vector<vector<int> > ans=s.threeSum(num);
     for(i=0;i<ans.size();i++){
