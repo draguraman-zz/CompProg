c6a9b8361b6bd2c8753991de5d8fe7fc89cc2a46
Lucas Tan <tbhtan3@gmail.com>
2014-09-17 21:20:16 +0800

    fixed divide, and solved others

--- LeetCode/divide.cpp
+++ LeetCode/divide.cpp
@@ -8,9 +8,7 @@ typedef long long ll;
 class Solution {
 public:
     int divide(int n_, int d_) {
-        ll ans=0,sign=1;
-        if((n_<0&&d_>=0)||(n_>=0&&d_<0))
-            sign=-1;
+        ll ans=0;
         ll n=abs((ll)n_);
         ll d=abs((ll)d_);
         while(n>=d){
@@ -20,17 +18,19 @@ public:
             ans+=m;
             n-=a;
         }
-        return ans*sign;
+        if((n_<0&&d_>=0)||(n_>=0&&d_<0))
+            return -ans;
+        return ans;
     }
 };
 
 int main(){
     int i,j;
     Solution s;
-    cout<<s.divide(2147483647,1)<<endl;
-    cout<<s.divide(-1010369383, -2147483648)<<endl;
-    cout<<s.divide(-2147483647, -1)<<endl;
-    cout<<s.divide(-2147483648, 1)<<endl;
+    cout<<(2147483647==s.divide(2147483647,1))<<endl;
+    cout<<(0==s.divide(-1010369383, -2147483648))<<endl;
+    cout<<(2147483647==s.divide(-2147483647, -1))<<endl;
+    cout<<(-2147483648==s.divide(-2147483648, 1))<<endl;
     for(i=-10000;i<10000;i++){
         for(j=1;j<1000;j++){
             if(s.divide(i,j)!=i/j){
